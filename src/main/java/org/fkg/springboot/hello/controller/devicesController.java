ba085a040667871cfde5939d4bacac580c1e34e4
hq1025 <hq10252002@aliyun.com>
2019-04-02 14:16:27 +0800

    添加控制器

--- src/main/java/org/fkg/springboot/hello/controller/devicesController.java
+++ src/main/java/org/fkg/springboot/hello/controller/devicesController.java
@@ -1,5 +1,20 @@
 package org.fkg.springboot.hello.controller;
 
-public class devicesController {
+import javax.annotation.Resource;
 
+import org.fkg.springboot.hello.service.HelloService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class devicesController {
+   
+    //定义业务层对象
+    @Resource
+    private HelloService helloService;   
+    
+    @RequestMapping("/getUsers")
+    public String getAllUsers(){
+       return helloService.getAllUsers(); 
+    }
 }
